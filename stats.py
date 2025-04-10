83b59ad7f3b0450da04da2f11df1ffba9148c223
Erenayas <153606353+Erenayas@users.noreply.github.com>
2025-04-10 13:25:23 +0300

    Create stats.py

---
+++ stats.py
@@ -0,0 +1,14 @@
+import string
+
+def count_characters(s):
+    return len(s.replace(" ", ""))
+
+def count_words(s):
+    return len(s.split())
+
+def average_word_length(s):
+    words = s.split()
+    if not words:
+        return 0
+    total_length = sum(len(word.strip(string.punctuation)) for word in words)
+    return total_length / len(words)    
